5f30c703c91d3430ac17acd8ac6a4a576c82e36f
Usman <51926991+UsmanAsifJrvs@users.noreply.github.com>
2019-08-06 10:01:42 -0400

    update

--- src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
+++ src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
@@ -4,9 +4,7 @@ import ca.jrvs.apps.trading.model.dto.SecurityRows;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import java.util.List;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "securityRows"
